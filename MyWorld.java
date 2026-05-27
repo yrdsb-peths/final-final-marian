c412e73a6298bd706cdea449cba0fcad197980a6
Xiangran217w57 <aha.marianli@gmail.com>
2026-05-27 11:30:58 -0400

    add pan speed

--- MyWorld.java
+++ MyWorld.java
@@ -1,14 +1,20 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    //background image size
     static final int SW = 1672;
     static final int SH = 941;
     
     static final int MAP_W = 1672;
     static final int MAP_H = 941;
     
+    //pan
     int panX = 0;
     int panY = 0;
+    static final int PAN_SPEED = 6;
+    
+    //zoom
+    
     
     public MyWorld()
     {
