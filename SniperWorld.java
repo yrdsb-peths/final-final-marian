6cb75f66461591293b7d0f575f161769b3492096
Xiangran217w57 <aha.marianli@gmail.com>
2026-06-05 13:22:56 -0400

    add

--- SniperWorld.java
+++ SniperWorld.java
@@ -19,6 +19,12 @@ public class SniperWorld extends World {
     int zoomviewW = (int)(SW/ZOOM_SCALE);
     int zoomviewH = (int)(SH/ZOOM_SCALE);
     
+    int zoomPanX = 0;
+    int zoomPanY = 0;
+    int prevMouseX = SW/2;
+    int prevMouseY = SH/2;
+    static final int ZOOM_PAN_SPEED = 4;
+    
     displayColumn hud;
     zoomView scope;
     CrosshairActor crosshair;
@@ -47,5 +53,67 @@ public class SniperWorld extends World {
         crosshair.setActive(false);
         
         startLevel(1);
+    }
+    
+    public void act()
+    {
+        if(gamePhase.equals("PLAYING"))
+        {
+            doPanning();
+            doZoom();
+            
+        }
+    }
+    
+    public void doPanning()
+    {
+        if(zoomed) return;
+        
+        int speed = PAN_SPEED;
+        
+        if (Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("a"))
+            panX -= speed;
+            
+        if (Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("d"))
+            panX += speed;
+            
+        if (Greenfoot.isKeyDown("up") || Greenfoot.isKeyDown("w"))
+            panX -= speed;
+            
+        if (Greenfoot.isKeyDown("down") || Greenfoot.isKeyDown("s"))
+            panX += speed;
+            
+        panX = clamp(panX, 0, MAP_W - SW);
+        panY = clamp(panY, 0, MAP_H - SH);
+        
+        for(Alien a : aliens)
+        {
+            a.setLocation(a.mapX - panX, a.mapY - panY);
+        }
+    }
+    
+    public void doZoom()
+    {
+        
+    }
+    
+    public void toggleZoom()
+    {
+        
+    }
+    
+    public void doShooting()
+    {
+        
+    }
+    
+    public void fireShot()
+    {
+        
+    }
+    
+    public void killAllien(Alien a)
+    {
+        
     }
 }
