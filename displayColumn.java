d9a960121a62835575f0160d38b0271281ab4dab
Xiangran217w57 <aha.marianli@gmail.com>
2026-06-07 19:53:31 -0400

    add refresh

--- displayColumn.java
+++ displayColumn.java
@@ -24,6 +24,28 @@ public class displayColumn extends Actor
     }
     public void act()
     {
-        // Add your action code here.
+        
+    }
+    
+    public void refresh()
+    {
+        GreenfootImage img = new GreenfootImage(W,H);
+        img.setColor(new Color(0,0,0,0));
+        img.fill();
+        
+        String phase = world.getPhase();
+        
+        if (phase.equals("PLAYING")) {
+            img.setColor(new Color(255, 255, 255, 160));
+            img.setFont(new Font("Times New Roman", false, false, 13));
+            img.drawString("Pan view  Z / RClick: Scope  Space / LClick: SHOOT at crosshair center", 10, 20);
+
+            if (world.isZoomed()) {
+                img.setColor(new Color(255, 60, 60, 230));
+                img.setFont(new Font("Times New Roman", true, false, 15));
+                String zs = "WASD to aim  |  Space / Click to FIRE at center dot";
+                img.drawString(zs, W / 2 - zs.length() * 4, 42);
+            }
+        }
     }
 }
