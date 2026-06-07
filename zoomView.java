ad6b2b154fd83c485a9ee1f5af9f84fb7e5d5577
Xiangran217w57 <aha.marianli@gmail.com>
2026-06-07 19:35:02 -0400

    finish zoomView

--- zoomView.java
+++ zoomView.java
@@ -85,19 +85,57 @@ public class zoomView extends Actor
         img.setColor(hairColor);
         
         //horizontal
-        img.drawline(cx-R+12, cy, cx-gap, cy);
-        img.drawline(cx+gap, cy, cx+R-12, cy);
+        img.drawLine(cx-R+12, cy, cx-gap, cy);
+        img.drawLine(cx+gap, cy, cx+R-12, cy);
         
         //vertical
-        img.drawline(cx, cy-R+12, cx, cy-gap);
-        img.drawline(cx, cy+gap, cx, cy+R-12);
+        img.drawLine(cx, cy-R+12, cx, cy-gap);
+        img.drawLine(cx, cy+gap, cx, cy+R-12);
         
         img.setColor(new Color(40, 220, 40, 100));
         img.drawLine(cx - R + 8, cy, cx + R - 8, cy);
         img.drawLine(cx, cy - R + 8, cx, cy + R - 8);
-    
+        
+        img.setColor(new Color(50, 210, 50, 180));
+        for (int i = -3; i <= 3; i++) {
+            if (i == 0) continue;
+            img.fillOval(cx + i * 42 - 3, cy - 3, 7, 7);
+            img.fillOval(cx - 3, cy + i * 42 - 3, 7, 7);
+        }
+        
+        img.setColor(new Color(50, 180, 50, 120));
+        for (int i = -3; i <= 3; i++) {
+            int tx = cx + i * 42;
+            int ty = cy + i * 42;
+            img.drawLine(tx, cy - 5, tx, cy + 5);
+            img.drawLine(cx - 5, ty, cx + 5, ty);
+        }
+        
+        img.setColor(new Color(255, 80, 80, 230));
+        img.fillOval(cx-4, cy-4, 8, 8);
+        img.setColor(new Color(255, 200, 200, 180));
+        img.fillOval(cx-1, cy-1, 3, 3);
+        
+        img.setFont(new Font("Monospaced", false, false, 9));
+        img.setColor(new Color(100, 240, 100, 140));
+        int[] ranges = {100, 200, 300, 400, 500, 600};
+        for (int i = 1; i <= 3; i++) {
+            if (i < ranges.length) {
+                img.drawString(String.valueOf(ranges[i-1]), cx + i * 42 + 4, cy - 5);
+                img.drawString(String.valueOf(ranges[i-1]), cx + 4, cy - i * 42 - 2);
+            }
+        }
+        
+        setImage(img);
     }
     
+    public void buildOff()
+    {
+        GreenfootImage img = new GreenfootImage(1, 1);
+        img.setColor(new Color(0,0,0,0));
+        img.fill();
+        setImage(img);
+    }
     
     
     
